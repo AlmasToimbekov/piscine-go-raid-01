fff6d19bdf015860f969e2a6332717f96ebd6119
Your Name <atoimbekov@gmail.com>
2019-10-18 19:39:17 +0600

    First ugly result

--- student/raid1a.go
+++ student/raid1a.go
@@ -1,13 +1,22 @@
 package student
 
+import "fmt"
+
 //Raid1a is drawing square area
 func Raid1a(x, y int) {
-	// matrix := [x][y]string{}
 
-	// fmt.Println(matrix[0[0]])
-	// for c := 0; c < x; c++ {
-	// 	for r := 0; r < y; r++ {
-	// 		if c == 0 && r == 0 || c == x - 1 && r ==
-	// 	}
-	// }
+	for col := 0; col < x; col++ {
+		for row := 0; row < y; row++ {
+			if col == 0 && row == 0 || col == x-1 && row == y-1 || col == 0 && row == y-1 || col == x-1 && row == 0 {
+				fmt.Print("o")
+			} else if col == 0 || col == x-1 {
+				fmt.Print("|")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		if col == x-1 {
+			fmt.Print("\n")
+		}
+	}
 }
