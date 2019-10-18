9a4504ff35cf1cdad3883819b84257c8b41e277a
Your Name <atoimbekov@gmail.com>
2019-10-18 18:57:13 +0600

    Sceleton for work

---
+++ student/raid1a.go
@@ -0,0 +1,13 @@
+package student
+
+//Raid1a is drawing square area
+func Raid1a(x, y int) {
+	// matrix := [x][y]string{}
+
+	// fmt.Println(matrix[0[0]])
+	// for c := 0; c < x; c++ {
+	// 	for r := 0; r < y; r++ {
+	// 		if c == 0 && r == 0 || c == x - 1 && r ==
+	// 	}
+	// }
+}
