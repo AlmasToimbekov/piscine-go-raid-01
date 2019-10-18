94e22f0e23a631b3afa707c0fae68c1bb4b901c5
Your Name <atoimbekov@gmail.com>
2019-10-18 20:12:52 +0600

    Close to end result of 1st task

--- student/raid1a.go
+++ student/raid1a.go
@@ -1,21 +1,31 @@
 package student
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/01-edu/z01"
+)
 
 //Raid1a is drawing square area
 func Raid1a(x, y int) {
 
-	for col := 0; col < x; col++ {
-		for row := 0; row < y; row++ {
-			if col == 0 && row == 0 || col == x-1 && row == y-1 || col == 0 && row == y-1 || col == x-1 && row == 0 {
-				fmt.Print("o")
-			} else if col == 0 || col == x-1 {
-				fmt.Print("|")
-			} else {
-				fmt.Print(" ")
-			}
+	for row := 0; row < y; row++ {
+		printRow(row, x, y)
+	}
+}
+
+func printRow(row int, x int, y int) {
+	for colomn := 0; colomn < x; colomn++ {
+		if colomn == 0 && row == 0 || colomn == x-1 && row == y-1 || colomn == 0 && row == y-1 || colomn == x-1 && row == 0 {
+			fmt.Print("o")
+		} else if colomn == 0 || colomn == x-1 {
+			fmt.Print("|")
+		} else if row == 0 || row == y-1 {
+			fmt.Print("-")
+		} else {
+			z01.PrintRune(' ')
 		}
-		if col == x-1 {
+		if colomn == x-1 {
 			fmt.Print("\n")
 		}
 	}
