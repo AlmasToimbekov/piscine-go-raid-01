91bfa0f9b86b151a6207c80de37a3c9b3dff6269
Your Name <atoimbekov@gmail.com>
2019-10-18 21:33:47 +0600

    Added third task's solution

---
+++ raid1c.go
@@ -0,0 +1,32 @@
+package student
+
+import (
+	"fmt"
+
+	"github.com/01-edu/z01"
+)
+
+//Raid1c is drawing square area from letters
+func Raid1c(x, y int) {
+
+	for row := 0; row < y; row++ {
+		printRowForC(row, x, y)
+	}
+}
+
+func printRowForC(row int, x int, y int) {
+	for colomn := 0; colomn < x; colomn++ {
+		if row == 0 && (colomn == x-1 || colomn == 0) {
+			fmt.Print("A")
+		} else if row == y-1 && (colomn == x-1 || colomn == 0) {
+			fmt.Print("C")
+		} else if colomn == 0 || colomn == x-1 || row == 0 || row == y-1 {
+			fmt.Print("B")
+		} else {
+			z01.PrintRune(' ')
+		}
+		if colomn == x-1 {
+			fmt.Print("\n")
+		}
+	}
+}
--- student/test.go
+++ student/test.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	student "./student"
+	student ".."
 )
 
 func main() {
-	student.Raid1a(5, 3)
+	student.Raid1c(5, 3)
 }
