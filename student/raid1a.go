41cd88e528937c3aa1cceed55efabf0905128991
bekmag <bekmag@github.com>
2019-10-18 20:40:00 +0600

    My first Raid1

--- student/raid1a.go
+++ student/raid1a.go
@@ -1,8 +1,6 @@
 package student
 
 import (
-	"fmt"
-
 	"github.com/01-edu/z01"
 )
 
@@ -17,16 +15,16 @@ func Raid1a(x, y int) {
 func printRow(row int, x int, y int) {
 	for colomn := 0; colomn < x; colomn++ {
 		if colomn == 0 && row == 0 || colomn == x-1 && row == y-1 || colomn == 0 && row == y-1 || colomn == x-1 && row == 0 {
-			fmt.Print("o")
+			z01.PrintRune('o')
 		} else if colomn == 0 || colomn == x-1 {
-			fmt.Print("|")
+			z01.PrintRune('|')
 		} else if row == 0 || row == y-1 {
-			fmt.Print("-")
+			z01.PrintRune('-')
 		} else {
 			z01.PrintRune(' ')
 		}
 		if colomn == x-1 {
-			fmt.Print("\n")
+			z01.PrintRune(10)
 		}
 	}
 }
