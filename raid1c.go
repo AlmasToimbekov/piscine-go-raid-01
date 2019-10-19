fabb5086889f68cc696553bc66f71470b667a981
Your Name <atoimbekov@gmail.com>
2019-10-19 12:28:37 +0600

    Get rid of fmt on raid1c

--- raid1c.go
+++ raid1c.go
@@ -1,8 +1,6 @@
 package student
 
 import (
-	"fmt"
-
 	"github.com/01-edu/z01"
 )
 
@@ -17,16 +15,16 @@ func Raid1c(x, y int) {
 func printRowForC(row int, x int, y int) {
 	for colomn := 0; colomn < x; colomn++ {
 		if row == 0 && (colomn == x-1 || colomn == 0) {
-			fmt.Print("A")
+			z01.PrintRune('A')
 		} else if row == y-1 && (colomn == x-1 || colomn == 0) {
-			fmt.Print("C")
+			z01.PrintRune('C')
 		} else if colomn == 0 || colomn == x-1 || row == 0 || row == y-1 {
-			fmt.Print("B")
+			z01.PrintRune('B')
 		} else {
 			z01.PrintRune(' ')
 		}
 		if colomn == x-1 {
-			fmt.Print("\n")
+			z01.PrintRune(10)
 		}
 	}
 }
